c15116f4b89264aba660575de7e01d110287558e
Rėjus Černiauskas <rejusvaldo123@google.com>
2022-01-20 17:23:16 +0200

    Darbo užbaigimas

---
+++ includes/hidden.php
@@ -0,0 +1,2 @@
+<?php
+echo "<input type=\"hidden\" name=\"todo_name\" value=\"" , $todoName , "\">";
--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+$todos = [];
+
+if (file_exists('todo.json'))
+{
+    $json = file_get_contents('todo.json');
+    $todos = json_decode($json, true);
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,9 +20,34 @@
     <title>Todo</title>
 </head>
 <body>
-    labas
-    <?php 
-        echo "TEST";
-    ?>
+    <form action="newTodo.php" method="POST">
+        <input type="text" name="todo_name" placeholder="Enter todo name">
+        <button>New todo</button>
+    </form>
+
+    <?php foreach ($todos as $todoName => $todo): ?>
+        <div>
+            <form action="change_status.php" method="POST">
+                <?php include "includes/hidden.php"; ?>
+                <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : ''; ?>>
+            </form>
+
+            <?php echo $todoName ?>
+
+            <form action="delete.php" method="POST">
+                <?php include "includes/hidden.php"; ?>
+                <button>Delete todo</button>
+            </form>
+        </div>
+    <?php endforeach; ?>
+
+<script>
+    const checkboxes = document.querySelectorAll('input[type=checkbox]')
+    checkboxes.forEach(ch => {
+        ch.onclick = function(){
+            this.parentNode.submit()
+        }
+    })
+</script>
 </body>
 </html>
